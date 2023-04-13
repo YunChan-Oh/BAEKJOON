3f16328f62b63791b32c088525ced39000baf24b
YunChan-Oh <oyunchan9@gmail.com>
2023-04-13 14:56:33 +0900

    1065 한수

--- 1065.c
+++ 1065.c
@@ -1,1 +1,37 @@
-coding
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+  char a[5];
+  scanf("%s", a);
+  int answer = 0, num = atoi(a);
+  for (int i = 1; i <= num; ++i) {
+    if (i < 100) {
+      answer++;
+    } 
+    else {
+      int g = i, p = 0;
+      while (g > 0) {
+        g /= 10;
+        p++;
+      }
+      char b[5];
+      sprintf(b,"%d",i);
+      int cnt = (b[0]-48) - (b[1]-48), te = 0;
+      for (int j = 1; j < p-1; ++j) {
+        if ((b[j]-48) - (b[j+1]-48) == cnt) {
+          te = 1;
+        } 
+        else {
+          te = 0;
+          break;
+        }
+      }
+      if (te) {
+        answer++;
+      }
+    }
+  }
+  printf("%d", answer);
+  return 0;
+}
